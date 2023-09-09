5f2e1ebb57c62e02d8d2a7c688fda95fd3cf1578
AnkitNayan83 <ankitnayan83@gmail.com>
2023-09-09 18:52:36 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,8 @@ export default function Home() {
   return (
     <main>
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-[32px] font-bold">Hii</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio unde maiores velit architecto sint ut necessitatibus blanditiis aliquid deserunt ab rerum eos expedita, officia neque quod voluptatem earum, a numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique mollitia corporis voluptatibus enim eligendi veniam fugiat cumque, dolores culpa eaque? Ex illum quo eveniet voluptatum molestiae quod explicabo non ut. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At est dolor, vitae qui incidunt hic fuga necessitatibus, reprehenderit quas eligendi magnam sit sequi molestias cupiditate. Sint tempore odio animi ab! Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto totam reprehenderit nam recusandae, itaque tempore enim, similique modi dignissimos soluta vero possimus tenetur doloremque quod? Laboriosam nisi eligendi excepturi voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, quam. Deserunt vel iste fuga ipsa magni odio totam numquam. Est iure saepe neque eum accusamus, blanditiis animi ex dolore veritatis?</p>
-        <Button>Test</Button>
+
+        <h1 className="text-[44px] md:text-[54px] mt-16"> <span className="font-bold">Are you up for some  GapShap 😊.</span> Discover great stories and creative ideas on variety of topics.</h1>
 
       </div>
     </main>
