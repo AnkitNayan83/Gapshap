5798f5346181ffb5303faf55dea43807c3504be7
AnkitNayan83 <ankitnayan83@gmail.com>
2023-09-09 11:02:12 +0530

    initialized project

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { ThemeProvider } from '@/components/theme-provider'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Navbar from '@/components/navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'GapShap',
+  description: 'GapShap is a blog app where you can share your stories realted to different topics',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-[32px] font-bold">Hii</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio unde maiores velit architecto sint ut necessitatibus blanditiis aliquid deserunt ab rerum eos expedita, officia neque quod voluptatem earum, a numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique mollitia corporis voluptatibus enim eligendi veniam fugiat cumque, dolores culpa eaque? Ex illum quo eveniet voluptatum molestiae quod explicabo non ut. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At est dolor, vitae qui incidunt hic fuga necessitatibus, reprehenderit quas eligendi magnam sit sequi molestias cupiditate. Sint tempore odio animi ab! Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto totam reprehenderit nam recusandae, itaque tempore enim, similique modi dignissimos soluta vero possimus tenetur doloremque quod? Laboriosam nisi eligendi excepturi voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, quam. Deserunt vel iste fuga ipsa magni odio totam numquam. Est iure saepe neque eum accusamus, blanditiis animi ex dolore veritatis?</p>
+        <Button>Test</Button>
+
+      </div>
+    </main>
+  );
+}
