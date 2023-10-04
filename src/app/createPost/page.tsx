fae02431a1198fbf6e092b906fc5bf7ab59ac86a
AnkitNayan83 <ankitnayan83@gmail.com>
2023-10-04 19:04:44 +0530

    commit

--- src/app/createPost/page.tsx
+++ src/app/createPost/page.tsx
@@ -3,17 +3,60 @@
 import ArrowBackIos from "@mui/icons-material/ArrowBackIos";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import React, { useState } from "react";
+import CancelIcon from "@mui/icons-material/Cancel";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
     const [cat, setCat] = useState("");
     const [title, setTitle] = useState("");
     const [desc, setDesc] = useState("");
+    const [file, setFile] = useState<File>();
+    const [text, setText] = useState("");
+
+    const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files) return;
+        else {
+            setFile(e.target.files[0]);
+        }
+    };
 
     return (
-        <div className="h-[calc(100vh_-_110px)]">
+        <div>
             {/* top */}
-            <div className="flex items-center justify-between">
+            <div>
                 <ArrowBackIos className="text-[32px] cursor-pointer" />
+            </div>
+
+            {/* title */}
+            <div className="mt-12">
+                <input
+                    type="text"
+                    placeholder="Title"
+                    className="text-[50px] bg-transparent border-none placeholder:text-gray-500 outline-none w-full"
+                />
+            </div>
+
+            {/* image */}
+
+            <div className="mt-6 flex items-center justify-between">
+                {file ? (
+                    <div className="flex items-center gap-4 text-2xl">
+                        <h1>{file.name}</h1> <CancelIcon onClick={() => setFile(undefined)} />{" "}
+                    </div>
+                ) : (
+                    <label htmlFor="file" className="cursor-pointer text-2xl">
+                        <FileUploadIcon className="text-gray-500 text-[32px]" /> Add Image
+                    </label>
+                )}
+                <input
+                    type="file"
+                    name="file"
+                    id="file"
+                    className="hidden"
+                    onChange={handelChange}
+                />
                 <div>
                     <select
                         name="categories"
@@ -45,21 +88,22 @@ const page = () => {
                 </div>
             </div>
 
-            {/* title */}
-            <div className="mt-12">
-                <input
-                    type="text"
-                    placeholder="Title"
-                    className="text-[50px] bg-transparent border-none placeholder:text-gray-500 outline-none"
+            {/* textarea */}
+
+            <div className="h-[20vh] mt-6">
+                <ReactQuill
+                    theme="snow"
+                    value={text}
+                    onChange={setText}
+                    className="h-[100%] text-[24px]"
                 />
             </div>
 
-            <div className="mt-6">
-                <label htmlFor="file" className="cursor-pointer">
-                    <FileUploadIcon className="text-gray-500 text-[32px]" /> Add Image
-                </label>
-                <input type="file" name="file" id="file" className="hidden" />
-            </div>
+            {/* postButton */}
+
+            <Button className="bg-green-400 hover:bg-green-500 mt-20 mb-12 w-[15%] text-[hsl(var(--foreground))] text-xl">
+                Post
+            </Button>
         </div>
     );
 };
